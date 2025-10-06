c4294aa16f5595a4feca9a9214b38ad70e91211a
maigol123 <migshenrique040507@gmail.com>
2025-10-05 21:21:39 -0300

    Refatoração da página de integrantes

--- access-control-cp/src/routes/Integrantes/index.tsx
+++ access-control-cp/src/routes/Integrantes/index.tsx
@@ -1,7 +1,25 @@
-export default function Integrantes(){
-    return(
-        <main>
-            <h1>Página de Integrantes</h1>
-        </main>
-    )
+import { useEffect } from "react";
+import CardIntegrante from "../../components/CardIntegrantes/CardIntegrantes";
+import { membros } from "../../Data/Integrantes";
+
+export default function Integrantes() {
+  useEffect(() => {
+    document.title = "CP5";
+  }, []);
+
+  return (
+    <main className="pagina_integrantes_main">
+      
+      <section className="pagina_integrantes_cabecalho">
+        <h2>Membros</h2>
+      </section>
+
+      
+      <section className="grid_integrantes">
+        {membros.map((membro) => (
+          <CardIntegrante key={membro.rm} {...membro} />
+        ))}
+      </section>
+    </main>
+  );
 }
